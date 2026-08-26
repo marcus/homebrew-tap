d0fd7b8324728aeab4be8841f0d6d7e2b8c2925b
Marcus Vorwaller <marcus@vorwaller.net>
2026-08-26 11:36:52 -0700

    tasks v1.15.0

--- Formula/tasks.rb
+++ Formula/tasks.rb
@@ -1,8 +1,8 @@
 class Tasks < Formula
   desc "Local-first GTD task system for human and AI co-working"
   homepage "https://github.com/marcus/tasks"
-  url "https://github.com/marcus/tasks/archive/refs/tags/v1.14.0.tar.gz"
-  sha256 "ca65e5d6ed1a348425437bfd682efa66bf80e7db160e9ace21078ae3eff65e31"
+  url "https://github.com/marcus/tasks/archive/refs/tags/v1.15.0.tar.gz"
+  sha256 "d4330e3abc1a57d7dcfb982ed2377d300fd0c67deaa8eee9990dc9ca5281bff3"
   license "MIT"
   head "https://github.com/marcus/tasks.git", branch: "main"
 
@@ -12,7 +12,7 @@ class Tasks < Formula
     ldflags = [
       "-s",
       "-w",
-      "-X github.com/marcus/tasks/internal/buildinfo.Version=v1.14.0",
+      "-X github.com/marcus/tasks/internal/buildinfo.Version=v1.15.0",
       "-X github.com/marcus/tasks/internal/buildinfo.Commit=homebrew",
     ].join(" ")
     system "go", "build", *std_go_args(output: bin/"tasks", ldflags:), "./cmd/tasks"
@@ -21,8 +21,8 @@ class Tasks < Formula
   end
 
   test do
-    assert_match "tasks v1.14.0 (homebrew)", shell_output("#{bin}/tasks --version")
-    assert_match "tasks-api v1.14.0 (homebrew)", shell_output("#{bin}/tasks-api --version")
-    assert_match "tasks-tui v1.14.0 (homebrew)", shell_output("#{bin}/tasks-tui --version")
+    assert_match "tasks v1.15.0 (homebrew)", shell_output("#{bin}/tasks --version")
+    assert_match "tasks-api v1.15.0 (homebrew)", shell_output("#{bin}/tasks-api --version")
+    assert_match "tasks-tui v1.15.0 (homebrew)", shell_output("#{bin}/tasks-tui --version")
   end
 end
