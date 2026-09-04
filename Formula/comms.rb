d563ba3423c297d6d36fab19806965b93f26eb68
Marcus Vorwaller <marcus@vorwaller.net>
2026-09-04 10:16:16 -0700

    comms v1.1.0

--- Formula/comms.rb
+++ Formula/comms.rb
@@ -1,8 +1,8 @@
 class Comms < Formula
   desc "Short-lived local messaging for independent agent sessions"
   homepage "https://github.com/marcus/comms"
-  url "https://github.com/marcus/comms/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "01767e098085e17e6a6e97aa01c0924fcc65bb76ac816dc67e623c32bedf6505"
+  url "https://github.com/marcus/comms/archive/refs/tags/v1.1.0.tar.gz"
+  sha256 "4433aac194894fe42f7fffd38fdfe5a2ecbbe823198d6f95fe8631929042704e"
   license "Apache-2.0"
   head "https://github.com/marcus/comms.git", branch: "main"
 
@@ -13,13 +13,21 @@ class Comms < Formula
     ldflags = [
       "-s",
       "-w",
-      "-X github.com/marcus/comms/pkg/buildinfo.Version=v1.0.0",
+      "-X github.com/marcus/comms/pkg/buildinfo.Version=v1.1.0",
       "-X github.com/marcus/comms/pkg/buildinfo.Commit=homebrew",
     ].join(" ")
     system "go", "build", *std_go_args(output: bin/"comms", ldflags:), "./cmd/comms"
   end
 
+  service do
+    run [opt_bin/"comms", "serve", "--supervised"]
+    run_at_load true
+    keep_alive true
+    log_path var/"log/comms.log"
+    error_log_path var/"log/comms.err.log"
+  end
+
   test do
-    assert_match "comms v1.0.0 (homebrew)", shell_output("#{bin}/comms version")
+    assert_match "comms v1.1.0 (homebrew)", shell_output("#{bin}/comms version")
   end
 end
